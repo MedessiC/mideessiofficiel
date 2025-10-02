14008cbd6dd929c2ef15f15a7cd7a8b9a82f782a
MedessiC <vivotinc@gmail.com>
2025-10-02 10:02:29 +0100

    creation de 404 not found

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -63,17 +63,32 @@ const NotFound = () => {
             </div>
           </div>
 
-          <div className="relative">
-            <div className="relative z-10">
+          {/* Image pour grand écran */}
+          <div className="relative hidden lg:block">
+            <div className="relative z-10 max-w-md mx-auto">
               <img
-                src="https://images.pexels.com/photos/4195325/pexels-photo-4195325.jpeg?auto=compress&cs=tinysrgb&w=800"
+                src="/404-image.png"
                 alt="Page non trouvée"
-                className="rounded-lg shadow-2xl"
+                className="rounded-lg shadow-2xl w-full h-auto object-contain"
               />
               <div className="absolute inset-0 bg-gradient-to-tr from-midnight/20 to-transparent rounded-lg"></div>
             </div>
-            <div className="absolute -top-4 -right-4 w-72 h-72 bg-gold/20 rounded-full blur-3xl"></div>
-            <div className="absolute -bottom-4 -left-4 w-64 h-64 bg-blue-500/20 rounded-full blur-3xl"></div>
+            <div className="absolute -top-4 -right-4 w-48 h-48 bg-gold/20 rounded-full blur-3xl"></div>
+            <div className="absolute -bottom-4 -left-4 w-40 h-40 bg-blue-500/20 rounded-full blur-3xl"></div>
+          </div>
+          
+          {/* Image pour mobile/tablette */}
+          <div className="relative lg:hidden mt-8">
+            <div className="relative z-10 max-w-sm mx-auto">
+              <img
+                src="/404-image.png"
+                alt="Page non trouvée"
+                className="rounded-lg shadow-xl w-full h-auto object-contain"
+              />
+              <div className="absolute inset-0 bg-gradient-to-tr from-midnight/20 to-transparent rounded-lg"></div>
+            </div>
+            <div className="absolute -top-2 -right-2 w-32 h-32 bg-gold/20 rounded-full blur-2xl"></div>
+            <div className="absolute -bottom-2 -left-2 w-28 h-28 bg-blue-500/20 rounded-full blur-2xl"></div>
           </div>
         </div>
 
